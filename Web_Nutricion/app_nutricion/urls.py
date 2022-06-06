99c622354f7caf522edf1299ec9e8d38f46cddaf
Lucas Nowicki <lucasnowicki11@gmail.com>
2022-06-05 21:30:34 -0300

    correccion de error en template evaluacion antropometrica, redireccion

--- Web_Nutricion/app_nutricion/urls.py
+++ Web_Nutricion/app_nutricion/urls.py
@@ -3,5 +3,5 @@ from app_nutricion.views import listar_clientes, evaluacion_antropometrica
 
 urlpatterns = [
     path('',listar_clientes, name='lista_clientes'),
-    path("", evaluacion_antropometrica, name="evaluacion_antropometrica"),
+    path('evaluacion', evaluacion_antropometrica, name="evaluacion_antropometrica"),
 ]

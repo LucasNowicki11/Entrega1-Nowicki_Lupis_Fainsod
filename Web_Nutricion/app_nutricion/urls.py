961880649e2eaed7289a63e1d29121c612133e3a
Lucas Nowicki <lucasnowicki11@gmail.com>
2022-06-07 00:04:55 -0300

    correcion de errores evaluacion antropometrica

--- Web_Nutricion/app_nutricion/urls.py
+++ Web_Nutricion/app_nutricion/urls.py
@@ -3,7 +3,7 @@ from app_nutricion.views import listar_clientes, evaluacion_antropometrica, carg
 
 urlpatterns = [
     path('',listar_clientes, name='lista_clientes'),
-    path('evaluacion', evaluacion_antropometrica, name="evaluacion_antropometrica"),
+    path('evaluacion/', evaluacion_antropometrica, name="evaluacion_antropometrica"),
     path('cargar-clientes/', cargar_clientes, name='cargar_clientes'),
     path('recetas/', recetas, name='recetas'),
 ]

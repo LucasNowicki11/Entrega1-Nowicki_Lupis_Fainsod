b54a634f9dc688ec45c4e0375fddc202e4ea4da0
leonel lupis <leonellupis@outlook.com>
2022-06-07 12:35:49 -0300

    Crud

--- Web_Nutricion/app_nutricion/admin.py
+++ Web_Nutricion/app_nutricion/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from app_nutricion.models import Clientes, Evaluacion_Antropometrica, Recetas
 # Register your models here.
 
-admin.site.register(Clientes)
-admin.site.register(Evaluacion_Antropometrica)
-admin.site.register(Recetas)
+@admin.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ["name", "last_name", "sexo" , "age"]
 
+@admin.register(Evaluacion_Antropometrica)
+class Evaluacion_AntropometricaAdmin(admin.ModelAdmin):
+    list_display = ["name_evaluacion", "age", "size", "IMC"]
+
+@admin.register(Recetas)
+class RecetasAdmin(admin.ModelAdmin):
+    list_display = ["recipe_name", "ingredients", "number_of_grams"]
 

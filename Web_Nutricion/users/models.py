8e639c06d82eda05bd76bc692ceeacf497c01783
leonel lupis <leonellupis@outlook.com>
2022-06-24 17:34:15 -0300

    agregacion de email al users

--- Web_Nutricion/users/models.py
+++ Web_Nutricion/users/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class User_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
     phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
     image = models.ImageField(upload_to = "profile_image")
